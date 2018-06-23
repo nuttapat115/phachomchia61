092a8d3e9e1b71e0048800008679be3a30b9c61c
meenkx <tyamdej@gmail.com>
2018-06-23 23:25:54 +0700

    update login page

--- web/WEB/resources/views/auth/login.blade.php
+++ web/WEB/resources/views/auth/login.blade.php
@@ -1,57 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+    <div style="
+    height: 100vh;
+    margin: -1.5rem 0px;
+      z-index: 1;
+      background-color: rgba(188, 143, 143, 0.5);
+      font-size: 50px;
+      text-align: center;
+      line-height: 100px;
+      box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
+">
+
+    </div>
+<div class="container-fluid" style="
+    position: fixed;
+    top: 0px;
+">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <img src="{{ asset('img/logo.png') }}" alt="" width="600px" height="400px" class="responsive">
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group row">
+                    <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <input id="password" type="hidden" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="EIEI" required>
-
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @endif
+                    </div>
                 </div>
-            </div>
+            </form>
+        </div>
         </div>
     </div>
 </div>
