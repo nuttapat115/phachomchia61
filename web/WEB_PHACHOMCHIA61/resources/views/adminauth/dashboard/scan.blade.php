204c12185dfb9fc15c49810e89d18d47849e20d9
meenkx <tyamdej@gmail.com>
2018-07-14 16:01:53 +0700

    fix scan

--- web/WEB_PHACHOMCHIA61/resources/views/adminauth/dashboard/scan.blade.php
+++ web/WEB_PHACHOMCHIA61/resources/views/adminauth/dashboard/scan.blade.php
@@ -3,10 +3,9 @@
 @section('content')
    <div class="container-fluid">
       <div class="row">
-          {{--<div class="embed-responsive embed-responsive-16by9">--}}
-              {{--<iframe class="embed-responsive-item" src="https://schmich.github.io/instascan/" allowfullscreen></iframe>--}}
-          {{--</div>--}}
-          scan test page
+          <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="https://meenkx.github.io/ScanPhachomchia61.github.io/" allowfullscreen></iframe>
+          </div>
       </div>
    </div>
 
