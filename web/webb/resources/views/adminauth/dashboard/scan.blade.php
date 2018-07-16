2e5865c88088d9be3e94d1d83e0ba744857827ac
meenkx <tyamdej@gmail.com>
2018-07-16 22:34:03 +0700

    update

---
+++ web/webb/resources/views/adminauth/dashboard/scan.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+   <div class="container-fluid">
+      <div class="row">
+          <div class="embed-responsive embed-responsive-16by9">              
+	        {{--<iframe class="embed-responsive-item" allow="geolocation; microphone; camera" src="https://meenkx.github.io/ScanPhachomchia61.github.io/" allowfullscreen></iframe>--}}
+	        <iframe class="embed-responsive-item" allow="geolocation; microphone; camera" src="https://meenkx.github.io/ScanPhachomchia61.github.io/" allowfullscreen></iframe>
+          </div>
+      </div>
+   </div>
+@endsection
